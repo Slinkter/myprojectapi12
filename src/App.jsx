c02bbf637d15c487293c757b8f08a2c8e0753b9c
Slinkter <luis.j.cueva@gmail.com>
2024-04-20 11:34:16 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <p className="font-serif ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+        aspernatur, quod molestiae blanditiis, mollitia, veritatis delectus iure
+        non corporis magni laboriosam quas adipisci? Fugiat non, esse officia
+        corrupti dignissimos sunt?
+      </p>
+    </div>
+  );
 };
 
 export default App;
