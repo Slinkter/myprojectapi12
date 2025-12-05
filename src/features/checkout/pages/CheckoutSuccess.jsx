08fe8751672f32ec0814b594d7c1b82b0f6bd977
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-12-05 12:19:56 -0500

    ook
    ok

--- src/features/checkout/pages/CheckoutSuccess.jsx
+++ src/features/checkout/pages/CheckoutSuccess.jsx
@@ -1,24 +1,28 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { Button, Card, Typography } from "@material-tailwind/react";
 
 const CheckoutSuccess = () => {
-    return (
-        <div className="checkout-success-page">
-            <Card className="checkout-success-card">
-                <Typography variant="h4" color="green" className="checkout-success-card__title">
-                    Payment Successful!
-                </Typography>
-                <Typography className="checkout-success-card__message">
-                    Thank you for your purchase.
-                </Typography>
-                <Link to="/">
-                    <Button variant="gradient" color="blue">
-                        Continue Shopping
-                    </Button>
-                </Link>
-            </Card>
-        </div>
-    );
+  return (
+    <div className="checkout-success-page">
+      <Card className="checkout-success-card">
+        <Typography
+          variant="h4"
+          color="green"
+          className="checkout-success-card__title"
+        >
+          Payment Successful!
+        </Typography>
+        <Typography className="checkout-success-card__message">
+          Thank you for your purchase.
+        </Typography>
+        <Link to="/">
+          <Button variant="gradient" color="blue">
+            Continue Shopping
+          </Button>
+        </Link>
+      </Card>
+    </div>
+  );
 };
 
 export default CheckoutSuccess;
