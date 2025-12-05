33a41545da5f00088c3d04e045e291a754ea69f7
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-12-05 09:46:34 -0500

    delete

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import { ThemeProvider } from "@/context/ThemeContext";
 import { CartProvider } from "@/context/CartContext";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <BrowserRouter basename="/myprojectapi12/">
-            <ThemeProvider>
-                <CartProvider>
-                    <App />
-                </CartProvider>
-            </ThemeProvider>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/myprojectapi12/">
+      <ThemeProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
