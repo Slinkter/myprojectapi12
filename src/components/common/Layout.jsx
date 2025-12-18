78f142b890d15a36b596f1b3c09591e0ca63e7bf
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-12-18 10:23:45 -0500

    ok
    ok

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -5,23 +5,23 @@ import PropTypes from "prop-types";
 import { CartContext } from "@/features/cart/context/CartContext";
 
 const Layout = ({ children }) => {
-    const { toggleCart } = useContext(CartContext);
+  const { toggleCart } = useContext(CartContext);
 
-    return (
-        <div className="layout">
-            <div className="layout__container">
-                <div className="layout__controls">
-                    <ThemeSwitcher />
-                    <CartIcon onClick={toggleCart} />
-                </div>
-                {children}
-            </div>
+  return (
+    <div className="layout">
+      <div className="layout__container">
+        <div className="layout__controls">
+          <ThemeSwitcher />
+          <CartIcon onClick={toggleCart} />
         </div>
-    );
+        {children}
+      </div>
+    </div>
+  );
 };
 
 Layout.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
