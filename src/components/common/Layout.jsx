1c0c4232cbd723fe7c3cd30c21244828de9f3958
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-12-24 23:02:31 -0500

    Checkpoint: Pre-Neumorphism UI Refactor

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -3,26 +3,36 @@ import PropTypes from "prop-types";
 import ThemeSwitcher from "./ThemeSwitcher";
 import CartIcon from "@/features/cart/presentation/CartIcon";
 import { CartContext } from "@/features/cart/application/CartContext";
+import { useEffect } from "react";
 
 const Layout = ({ children }) => {
-    //
-    const { toggleCart } = useContext(CartContext);
+  //
+  const { toggleCart } = useContext(CartContext);
 
-    return (
-        <div className="layout">
-            <div className="layout__container">
-                <div className="layout__controls">
-                    <ThemeSwitcher />
-                    <CartIcon onClick={toggleCart} />
-                </div>
-                {children}
-            </div>
+  function testing() {
+    console.log("testing");
+  }
+
+  useEffect(() => {
+    testing();
+    return () => {};
+  }, []);
+
+  return (
+    <div className="layout">
+      <div className="layout__container">
+        <div className="layout__controls">
+          <ThemeSwitcher />
+          <CartIcon onClick={toggleCart} />
         </div>
-    );
+        {children}
+      </div>
+    </div>
+  );
 };
 
 Layout.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
--- src/features/cart/presentation/CartIcon.jsx
+++ src/features/cart/presentation/CartIcon.jsx
@@ -5,20 +5,20 @@ import PropTypes from "prop-types";
 import { HiOutlineShoppingCart } from "react-icons/hi2";
 
 const CartIcon = ({ onClick }) => {
-    const { cart } = useContext(CartContext);
-    const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+  const { cart } = useContext(CartContext);
+  const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
 
-    return (
-        <Badge content={totalItems > 0 ? totalItems : null} withBorder>
-            <IconButton onClick={onClick}>
-                <HiOutlineShoppingCart className="cart-icon__svg h-5 w-5" />
-            </IconButton>
-        </Badge>
-    );
+  return (
+    <Badge content={totalItems > 0 ? totalItems : null} withBorder>
+      <IconButton onClick={onClick}>
+        <HiOutlineShoppingCart className="cart-icon__svg h-5 w-5" />
+      </IconButton>
+    </Badge>
+  );
 };
 
 CartIcon.propTypes = {
-    onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default CartIcon;
